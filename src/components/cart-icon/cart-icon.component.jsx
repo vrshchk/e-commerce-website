9fce55f9d6e06a6758b589e20d247f1563286521
vrshchk <maryvaresh12@gmail.com>
2020-10-31 17:34:55 +0200

    cart dropdown added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,10 +4,10 @@ import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart-actions'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -15,4 +15,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
